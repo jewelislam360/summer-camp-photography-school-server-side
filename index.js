69d593cdbc4a66a5ee74eca5a68b6b4f4a4230b0
jewelislam360 <jewelislam.it360@gmail.com>
2023-06-11 02:38:13 +0600

    today last commit

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -34,9 +51,9 @@ async function run() {
     const userCollection = client.db("photographyDb").collection("users");
 
 
-    app.post('/jwt',(req, res)=>{
+    app.post('/jwt', (req, res)=>{
       const user =req.body;
-      const token =  jwt.sign(user, env.process.ACCESS_TOKEN_SECRET, {expireIn: '1h'})
+      const token =  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
       res.send(token);
     })
 
@@ -60,6 +77,21 @@ async function run() {
 
     })
 
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+
+      // if (req.decoded.email !== email) {
+      //   res.send({ admin: false })
+      // }
+
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+
+   
+
     app.patch('/users/admin/:id', async(req,res)=>{
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
@@ -72,6 +104,19 @@ async function run() {
       res.send(result);
 
 
+    })
+
+    app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ instructor: false })
+      }
+
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      const result = { instructor: user?.role2 === 'instructor' }
+      res.send(result);
     })
 
   
