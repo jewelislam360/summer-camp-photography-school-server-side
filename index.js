d3c87f5f7a5b9b5d0f28d51969d5bff444c2b01b
jewelislam360 <jewelislam.it360@gmail.com>
2023-06-14 01:36:43 +0600

    payment add

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-
 require('dotenv').config();
 
 const app=express();
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -50,6 +50,9 @@ async function run() {
    
     const userCollection = client.db("photographyDb").collection("users");
     const addclassCollection = client.db("photographyDb").collection("addclass");
+    const selectedClassCollection = client.db("photographyDb").collection("selectedClass");
+    const paymentCollection = client.db("photographyDb").collection("payments");
+
 
 
     app.post('/jwt', (req, res)=>{
@@ -108,7 +111,7 @@ async function run() {
     })
 
     app.get('/users/instructor/:email', async (req, res) => {
-      console.log("hello");
+
       const email = req.params.email;
 
       // if (req.decoded.email !== email) {
@@ -206,6 +209,72 @@ async function run() {
       const result = await userCollection.find(query).toArray();
       res.send(result);
     })
+
+    app.post('/selectedclass', async(req, res)=>{
+      const selectClass=req.body;
+      const result=await selectedClassCollection.insertOne(selectClass);
+      res.send(result);
+
+    })
+    app.get('/selectedclass/:email', async(req, res)=>{
+      const email = req.params.email;
+      const query={email: email};
+      const result = await selectedClassCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.get('/payment/:id', async(req, res)=>{
+      const id= req.params.id;
+      const filter= {_id: new ObjectId(id)}
+      const result= await selectedClassCollection.findOne(filter);
+      res.send(result);
+    } )
+
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const insertResult = await paymentCollection.insertOne(payment);
+
+      const query = {_id: new ObjectId(payment.paymentClassId)}
+
+      const deleteResult = await selectedClassCollection.deleteMany(query)
+      const filter = {_id: new ObjectId(payment.classId)}
+      const filterClass = await addclassCollection.findOne(filter);
+      if(filterClass){
+        const sitUpdate= filterClass.availableSeats-1 ;
+        // const enrolledStudents = filterClass.enrolledStudent+1 ;
+        const updateDoc ={
+          $set:{
+            availableSeats: sitUpdate,
+            // enrolledStudent: enrolledStudents,
+
+          }
+        }
+        const updateResult = await addclassCollection.updateOne(filter,updateDoc);
+        res.send({insertResult, deleteResult, updateResult })
+      
+        
+      }
+
+      else{
+        res.status(404).send("data not found")
+      }
+    })
+
+
 
 
 
